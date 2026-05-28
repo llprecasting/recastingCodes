d62f974950d8672fcb5c4ad24d011e0f03850857
Andre Lessa <lessa.a.p@gmail.com>
2026-05-28 13:12:27 -0300

    Removed env check

--- DisplacedLeptons/ATLAS-SUSY-2018-14/runScanMG5_worker.py
+++ DisplacedLeptons/ATLAS-SUSY-2018-14/runScanMG5_worker.py
@@ -19,15 +19,6 @@ if __name__ == "__main__":
     ap.add_argument('-c', '--configfile', required=True,
             help='path to the config file for this job, i.e. config.pkl')
     
-    # First make sure the correct env variables have been set:
-    LDPATH = subprocess.check_output('echo $LD_LIBRARY_PATH',shell=True,text=True)
-    ROOTINC = subprocess.check_output('echo $ROOT_INCLUDE_PATH',shell=True,text=True)
-    pythiaDir = os.path.abspath('./MG5/HEPTools/pythia8/lib')
-    delphesDir = os.path.abspath('./DelphesLLP/external')
-    if pythiaDir not in LDPATH or delphesDir not in ROOTINC:
-        print('Enviroment variables not properly set. Run source setenv.sh first.')
-        sys.exit()
-
     t0 = time.time()
     args = ap.parse_args()
     configFile = args.configfile
